5196547a3a69d00e56d339bda79d9447237a1a11
vantruong.fe42 <v4ntru0ng.bamboo@gmail.com>
2024-11-15 15:31:46 +0700

    changes end Todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
-import { PlusCircleOutlined } from "@ant-design/icons";
+import {
+  CheckOutlined,
+  DeleteOutlined,
+  PlusCircleOutlined,
+} from "@ant-design/icons";
 import "./app.scss";
-import { Button, Form, Input, Space } from "antd";
+import { Button, Form, Input, Pagination } from "antd";
 
 function App() {
   return (
@@ -10,10 +14,83 @@ function App() {
           <h2 className="todo-list-header__title">Todo list appliacation</h2>
           <Form className="todo-list-header__form">
             <Input size="large" placeholder="Please input task name..." />
-            <Button type="primary">
-              <PlusCircleOutlined />
+            <Button className="todo-list-header__bnt-add-task">
+              <PlusCircleOutlined style={{ fontSize: "30px" }} />{" "}
             </Button>
           </Form>
+          <div className="divider" />
+        </div>
+
+        <div className="todo-list-main">
+          <div className="task">
+            <p className="task__name">task 3 </p>
+            <div className="task__groups-btn">
+              <Button className="task__btn-done">
+                <CheckOutlined />
+              </Button>
+              <Button className="task__btn-del">
+                <DeleteOutlined />
+              </Button>
+            </div>
+          </div>
+          <div className="divider" />
+
+          <div className="task">
+            <p className="task__name">task 2 </p>
+            <div className="task__groups-btn">
+              <Button className="task__btn-done">
+                <CheckOutlined />
+              </Button>
+              <Button className="task__btn-del">
+                <DeleteOutlined />
+              </Button>
+            </div>
+          </div>
+          <div className="divider" />
+
+          <div className="task">
+            <p className="task__name">asdasdsadas </p>
+            <div className="task__groups-btn">
+              <Button className="task__btn-done">
+                <CheckOutlined />
+              </Button>
+              <Button className="task__btn-del">
+                <DeleteOutlined />
+              </Button>
+            </div>
+          </div>
+          <div className="divider" />
+
+          <div className="task">
+            <p className="task__name task__name--color">ahihi... </p>
+            <div className="task__groups-btn">
+              <Button className="task__btn-done">
+                <CheckOutlined />
+              </Button>
+              <Button className="task__btn-del">
+                <DeleteOutlined />
+              </Button>
+            </div>
+          </div>
+          <div className="divider" />
+
+          <div className="task">
+            <p className="task__name task__name--color">di ngu thoi </p>
+            <div className="task__groups-btn">
+              <Button className="task__btn-done">
+                <CheckOutlined />
+              </Button>
+              <Button className="task__btn-del">
+                <DeleteOutlined />
+              </Button>
+            </div>
+          </div>
+          <div className="divider" />
+        </div>
+
+        <div className="todo-list-pagination">
+          <div className="divider" />
+          <Pagination align="center" defaultCurrent={1} total={50} />
         </div>
       </div>
     </div>
